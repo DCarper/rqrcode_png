9d1dc813c17b1ca6d21117eac249fd71560003f5
dcarper <djcarper@me.com>
2011-10-08 12:48:48 -0400

    tidying up

--- lib/rqrcode_png.rb
+++ lib/rqrcode_png.rb
@@ -5,8 +5,4 @@ require "rqrcode_png/version"
 require "rqrcode_png/image"
 require "rqrcode_png/qrcode_extensions"
 
-module RQRCodePng
-  # Your code goes here...
-end
-
 RQRCode::QRCode.send :include, RQRCodePNG::QRCodeExtensions
